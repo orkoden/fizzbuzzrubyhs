26335439a5c8cf4631701f18a6677d7a4aeb1ab9
Jörg <joergbuehmann@gmail.com>
2014-11-22 19:11:12 +0100

    extracted more methods and added more tests

--- fizzbuzz_hackship.rb
+++ fizzbuzz_hackship.rb
@@ -1,22 +1,28 @@
 # FizzBuzz for Hackership
 # Jörg Bühmann joergbuehmann@gmail.com
 
+DIVISIBLE_BY_3_TEXT = "Hacker"
+DIVISIBLE_BY_5_TEXT = "Ship"
+
+def isNumberDivisibleBy(number, divider)
+  (number%divider == 0)
+end
+
 def fizzOrBuzzOrNumber (number)
-  divisibleBy3 = (number%3 == 0)
-  divisibleBy5 = (number%5 == 0)
-  if divisibleBy3 && divisibleBy5
-    printOut = "FizzBuzz"
-  elsif divisibleBy5
-    printOut = "Buzz"
-  elsif divisibleBy3
-    printOut = "Fizz"
-  else
-    printOut = number
-  end
+  divisibleBy3 = isNumberDivisibleBy(number, 3)
+  divisibleBy5 = isNumberDivisibleBy(number, 5)
+  printOut = ""
+  printOut = DIVISIBLE_BY_3_TEXT if divisibleBy3
+  printOut += DIVISIBLE_BY_5_TEXT if divisibleBy5
+  printOut = number if printOut.length == 0
   printOut
 end
 
-numbers = (1..100)
+def numbersFrom1to100
+  (1..100)
+end
+
+numbers = numbersFrom1to100
 numbers.each do |number|
   puts fizzOrBuzzOrNumber(number)
 end
--- tc_fizzbuzz_hackship.rb
+++ tc_fizzbuzz_hackship.rb
@@ -5,9 +5,21 @@ class TestFizzBuzz < Test::Unit::TestCase
  
   def test_fizzOrBuzzOrNumber
     assert_equal(4, fizzOrBuzzOrNumber(4) )
-    assert_equal("Fizz", fizzOrBuzzOrNumber(9) )
-    assert_equal("Buzz", fizzOrBuzzOrNumber(50) )
-    assert_equal("FizzBuzz", fizzOrBuzzOrNumber(45) )
+    assert_equal("Hacker", fizzOrBuzzOrNumber(9) ) # if it's divisible by 3, print "Hacker".
+    assert_equal("Ship", fizzOrBuzzOrNumber(50) ) # number is divisible by 5 instead of the number print "Ship".
+    assert_equal("HackerShip", fizzOrBuzzOrNumber(45) ) # Print "HackerShip" if the number is divisible by both 3 and 5.
   end
  
+  def test_isNumberDivisibleBy
+    assert_true(isNumberDivisibleBy(3,3))
+    assert_true(isNumberDivisibleBy(30,3))
+    assert_true(isNumberDivisibleBy(5,5))
+    assert_true(isNumberDivisibleBy(25,5))
+    assert_false(isNumberDivisibleBy(12,5))
+    assert_false(isNumberDivisibleBy(12,7))
+  end
+  
+  def test_numbersFrom1to100  # the numbers from 1 to 100 (inclusive)
+    assert_equal(numbersFrom1to100.count, 100)
+  end
 end
